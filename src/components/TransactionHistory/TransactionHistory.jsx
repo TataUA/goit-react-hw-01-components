7383f0bacb0014676418a78383e5c4799b9f18ca
TataUA <khyzhnjaktetjana@gmail.com>
2023-06-19 01:03:33 +0300

    hw1

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -25,7 +25,7 @@ export const TransactionHistory = ({items}) => {
                 //     })
                 .map(({id, type, amount, currency}) => (
                     <tr key={id}>
-                    <td>{type}</td>
+                    <td className={css.transaction_type}>{type}</td>
                     <td>{amount}</td>
                     <td>{currency}</td>
                 </tr>
