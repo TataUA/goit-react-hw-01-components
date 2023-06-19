f2c95677d2cfd36e54b338d8d6ca2eca0d95012e
TataUA <khyzhnjaktetjana@gmail.com>
2023-06-19 21:35:04 +0300

    hw changed

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,28 +1,29 @@
 import PropTypes from 'prop-types';
 import { FriendListItem } from 'components/FriendListItem/FriendListItem';
-import css from "components/FriendList/FriendList.module.css";
+import css from 'components/FriendList/FriendList.module.css';
 
-export const FriendList = ({friends}) => {
-    return (
+export const FriendList = ({ friends }) => {
+  return (
     <ul className={css.friend_list}>
-        {friends.map(({avatar, name, isOnline, id}) => (
-            <FriendListItem  
-                key={id}
-                avatar={avatar}
-                name={name}
-                isOnline={isOnline}
-            />
-        ))}
-    </ul>)
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
 };
 
-FriendList.propTypes ={
-    friends: PropTypes.arrayOf(
-      PropTypes.exact({
-        avatar: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        isOnline: PropTypes.bool.isRequired,
-        id: PropTypes.number.isRequired,
-      }),
-)};
-
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,36 +1,37 @@
 import PropTypes from 'prop-types';
-import css from "components/Statistics/Statistics.module.css";
-import {getRandomHexColor} from "utils/random-color.js";
+import css from 'components/Statistics/Statistics.module.css';
+import { getRandomHexColor } from 'utils/random-color.js';
 
-export const Statistics = ({title, stats}) => {  
-    return (
+export const Statistics = ({ title, stats }) => {
+  return (
     <section className={css.statistics}>
-      {title ? <h2 className={css.title} style={{
-          color: getRandomHexColor()}}>{title}</h2> : ""}    
-  
-      <ul className={css.stat_list}>
-        {stats.map(({id, label, percentage}) => (
-        <li className={css.item} key={id} style={{
-          backgroundColor: getRandomHexColor()
-        }}>
-          <span className={css.label}>{label}</span>
-          <span className={css.percentage}>{percentage}%</span>
-        </li>))}
-       </ul>
-    </section>)
-};
-       
+      {title && <h2 className={css.title}>{title}</h2>}
 
-Statistics.propTypes ={
-  data: PropTypes.arrayOf(
+      <ul className={css.stat_list}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            className={css.item}
+            key={id}
+            style={{
+              backgroundColor: getRandomHexColor(),
+            }}
+          >
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
     PropTypes.exact({
-    title: PropTypes.string, 
-    stats: PropTypes.exact({
       id: PropTypes.string.isRequired,
       label: PropTypes.string.isRequired,
       percentage: PropTypes.number.isRequired,
-    }),
-  }),    
-)};
-
-    
+    })
+  ).isRequired,
+};
